d0396b9049f30f0ffadae7f9081c2ad5cc89ca15
uyen <honguyen2996@gmail.com>
2018-10-25 08:54:15 +0700

    finish project

---
+++ app/src/main/java/com/example/uyen/tablayoutviewpager/Constants.java
@@ -0,0 +1,7 @@
+package com.example.uyen.tablayoutviewpager;
+
+public class Constants {
+    public static final String FRAG_FIRST = "First";
+    public static final String FRAG_SECOND = "Second";
+    public static final String FRAG_THIRD = "Third";
+}
--- app/src/main/java/com/example/uyen/tablayoutviewpager/MainActivity.java
+++ app/src/main/java/com/example/uyen/tablayoutviewpager/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.uyen.tablayoutviewpager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout mTabLayout = findViewById(R.id.tabs);
+        ViewPager mViewPager = findViewById(R.id.viewPager);
+
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new FirstFragment(), getResources().getString(R.string.first_title));
+        mAdapter.addFragment(new ThirdFragment(), getResources().getString(R.string.second_title));
+        mAdapter.addFragment(new SecondFragment(),getResources().getString(R.string.third_title));
+        mViewPager.setAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
